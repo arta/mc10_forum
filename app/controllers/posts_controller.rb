0fbe6db98fb193cd882da1bef5ee2202a4701f59
Martin Fencl <martin.fencl@gmail.com>
2017-01-18 14:12:52 -0600

    Links and filler text on /posts/index, /new, /_form and /show (with params[:id]) templates.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   #   =link_to .. posts_path
   #   get '/posts', to: 'posts#index'
   def  index
-    render plain:'This is /posts/index page.'
+
   end
   # Implicitly renders /posts/index haml view
 
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   #   =link_to .. new_post_path
   #   get '/posts/new', to:'posts#new', as: new_post(_path is implied)
   def new
-    render plain:'This is /posts/new page'
+
   end
   # Implicitly renders /posts/new haml view with /posts/_form partial
   #   =form_for @post .. with a @post.new_record? = true
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   #   get '/posts/:id', to: 'posts#show', as: post(_path is implied)
   #   router reads :id value from the request and assigns it to params[:id]
   def show
-    render plain:"This is /posts/show page; router set the value of params[:id] to #{params[:id]}."
+    
   end
   # Implicitly renders /posts/show haml view with @post :id available to it
 end
