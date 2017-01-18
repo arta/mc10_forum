910bbcc947a33ef1d57452cfdfe3459067379668
Martin Fencl <martin.fencl@gmail.com>
2017-01-18 14:29:46 -0600

    Create, show and list (index) posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   #   =link_to .. posts_path
   #   get '/posts', to: 'posts#index'
   def  index
-
+    @posts = Post.order created_at: :desc
   end
   # Implicitly renders /posts/index haml view
 
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   #   =link_to .. new_post_path
   #   get '/posts/new', to:'posts#new', as: new_post(_path is implied)
   def new
-
+    @post = Post.new
   end
   # Implicitly renders /posts/new haml view with /posts/_form partial
   #   =form_for @post .. with a @post.new_record? = true
@@ -23,10 +23,15 @@ class PostsController < ApplicationController
 
   # POST /posts
   #   <form action='/posts', method='post' ..>
-  #   =form_for @post .. if @post.new_record?
+  #   =form_for @post .. when @post.new_record?
   #   post '/posts', to: 'posts#create'
   def create
-
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post, notice:'Post created.'
+    else
+      render 'new'
+    end
   end
   # No view. Explicitly redirect_to|render action|view
 
@@ -36,7 +41,12 @@ class PostsController < ApplicationController
   #   get '/posts/:id', to: 'posts#show', as: post(_path is implied)
   #   router reads :id value from the request and assigns it to params[:id]
   def show
-    
+    @post = Post.find params[:id]
   end
   # Implicitly renders /posts/show haml view with @post :id available to it
+
+  private
+    def post_params
+      params.require( :post ).permit( :title, :content )
+    end
 end
