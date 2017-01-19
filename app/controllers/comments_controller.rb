382a7e7d92c7503d74bf1426fe8ebdbc2b9cfd7f
Martin Fencl <martin.fencl@gmail.com>
2017-01-19 16:16:36 -0600

    Post comments (associated with User) to Posts.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,15 @@ class CommentsController < ApplicationController
   #   haml:   =form_for [@post, @post.comments.new] ..
   #   router: post '/posts/:post_id/comments', to: 'comments#create'
   def create
-    
+    @post = Post.find params[:post_id]
+    comment = @post.comments.create( comment_params.merge( user:current_user ) )
+    flash[:notice] = 'Comment posted.' if comment
+    redirect_to @post
   end
   # No view. Excplicitly redirect_to|render action|view
+
+  private
+    def comment_params
+      params.require( :comment ).permit( :comment )
+    end
 end
